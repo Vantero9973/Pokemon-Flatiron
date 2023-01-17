0e3a29502b3988e5d7bae778ec06c4fe49a8762e
Alex Van Vleet <alexvan73@yahoo.com>
2023-01-16 17:32:52 -0700

    Design updates

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,29 +1,50 @@
 import { Link } from "react-router-dom";
 
 function NavBar() {
-
-    return (
-        <nav className="navBar">
-            <h1 className="siteTitle"><img className="logo" src="https://i.imgur.com/Ii8W4Uj.png" alt="logo" />Pokémon Flatiron</h1>
-            <ul>
-                <li>
-                    <Link to="/"><a href="/" className="pokeDexLink">PokéDex</a></Link>
-                </li>
-                <li>
-                    <Link to="/AddNewPokemon"><a href="/AddNewPokemon" className="pokeDexLink">Add New Pokémon</a></Link>
-                </li>
-                <li>
-                    <Link to="/PokeMart"><a href="/PokeMart" className="shopLink">PokéMart</a></Link>
-                </li>
-                <li>
-                    <Link to="/Checkout"><a href="/Checkout" className="shopLink">Checkout</a></Link>
-                </li>
-                <li>
-                    <Link to="/OrderHistory"><a href="/OrderHistory" className="shopLink">Order History</a></Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navBar">
+      <Link to="/">
+        <h1 className="siteTitle">
+          <img
+            className="logo"
+            src="https://i.imgur.com/Ii8W4Uj.png"
+            alt="logo"
+          />
+          Pokémon Flatiron
+        </h1>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/AddNewPokemon">
+            <a href="/AddNewPokemon" className="pokeDexLink">
+              Add New Pokémon
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/PokeMart">
+            <a href="/PokeMart" className="shopLink">
+              PokéMart
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/Checkout">
+            <a href="/Checkout" className="shopLink">
+              Checkout
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/OrderHistory">
+            <a href="/OrderHistory" className="shopLink">
+              Order History
+            </a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/OrderHistory.js
+++ src/OrderHistory.js
@@ -1,23 +1,34 @@
-import Card from '@mui/material/Card'
+import Card from "@mui/material/Card";
 
 function OrderHistory({ lastOrder }) {
-    console.log({ lastOrder })
-    return (
-        <div>
-            <h4 className="orderHistory" style={{ color: "gray" }}>Order Number: #{lastOrder.id}</h4>
-            {lastOrder.cart.map(pokemon => {
-                return (
-                    <>
-                        <Card className="orderHistory">
-                            <h4>{pokemon.name}</h4>
-                            <img src={pokemon.sprites} alt={pokemon.name} />
-                            <h5>${pokemon.price.toFixed(2)}</h5>
-                        </Card>
-                    </>
-                )
-            })}
-        </div>
-    )
+  console.log({ lastOrder });
+  return (
+    <div>
+      <h4 className="orderHistory" style={{ color: "gray" }}>
+        Order Number: #{lastOrder.id}
+      </h4>
+      {lastOrder.cart.map((pokemon) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Card
+              className="orderHistory"
+              style={{ width: "40vw", background: "lightgray" }}
+            >
+              <h4>{pokemon.name}</h4>
+              <img src={pokemon.sprites} alt={pokemon.name} />
+              <h5>${pokemon.price.toFixed(2)}</h5>
+            </Card>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default OrderHistory;
